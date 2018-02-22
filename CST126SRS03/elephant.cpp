75f07ac0d35d7c9c66163664ca9dd52c45e70947
Seadra <colin.rexing@gmail.com>
2018-02-22 13:36:00 -0800

    Starting psudo code

--- CST126SRS03/elephant.cpp
+++ CST126SRS03/elephant.cpp
@@ -1,2 +1,18 @@
 #include "stdafx.h"
+#include "elephant.h"
+#include "gps.h"
+#include "preserve.h"
+#include "direction.h"
+
+
+
+
+//while elephant hasnt found heard repeat these following steps
+// attach and update gps position if needed
+//
+// check for food / water
+// if food / water == true eat / drink
+// 
+// listen for heard
+
 
