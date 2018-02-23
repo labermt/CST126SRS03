98de8eca92e5dc99527d4d5ba3acedbff08ced5c
Seadra <colin.rexing@gmail.com>
2018-02-22 16:00:23 -0800

    Started programming, heading heard direction, and drinking water. Not sure how to check for water.

--- CST126SRS03/elephant.cpp
+++ CST126SRS03/elephant.cpp
@@ -1,18 +1,41 @@
 #include "stdafx.h"
 #include "elephant.h"
-#include "gps.h"
-#include "preserve.h"
-#include "direction.h"
 
+
+
+void Elephant::findHerd()
+{
+	auto found_heard = false;
+	while(!found_heard)
+	{
+		auto heading = getHeading(Turn::kForward);
+		auto heard_dir = listen();
+		if(water_ <= kMaxWater / 2 )
+		{
+			drink();
+		}
+		// food isnt implemented yet apparently 
+		// if(food_ <= kMaxFood / 2 )
+		// {
+		//	eat();
+		// }
+	}
+}
 
 
 
 //while elephant hasnt found heard repeat these following steps
 // attach and update gps position if needed
 //
+// setup following as case statements
+//
 // check for food / water
 // if food / water == true eat / drink
 // 
 // listen for heard
-
-
+// turn to closest direction of heard
+// 
+// look forward
+// if blocked && heard  < 90
+// turn right else turn left
+//
