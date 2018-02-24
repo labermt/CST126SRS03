7a05ced2d19b2c6fa64ab903d4a0b3750ea131fc
jaredmoit <jared.muralt@oit.edu>
2018-02-24 09:58:15 -0800

    Lab Work, cleanup / understanding

--- CST126SRS03/elephant.cpp
+++ CST126SRS03/elephant.cpp
@@ -1,101 +1,17 @@
 #include "stdafx.h"
-#include "elephant.h"
 
 #include "direction.h"
 #include "gps.h"
 #include "preserve.h"
 #include "elephant.h"
 
-class Elephant
+void Elephant::tag(GPS& gps)
 {
-public:
-	enum class Turn { k0, kForward, kLeft, kRight };
-
-private:
-	GPS * gps_{};
-
-private:
-	static constexpr unsigned kMaxAwake{ 22 }; // hours/day
-	static constexpr unsigned kMaxWater{ 200 }; // liters/day
-	static constexpr unsigned kMaxFood{ 200 }; // kg/day
-
-	unsigned clock_{}; // TODO: Make the clock run. 
-	unsigned awake_{};
-	unsigned water_{}; // [0, kMaxWater)
-
-	unsigned weight_; // [minWeight_,maxWeight_)
-
-	const unsigned minWeight_;
-	const unsigned maxWeight_;
-
-	Direction heading_;
-
-public:
-	Elephant(const unsigned weight, const Direction heading) 
-		:heading_{ heading }, weight_{ weight }, minWeight_{}, maxWeight_{}
-	{
-
-	};
-
-private:
-	static Preserve::Feature look(const Elephant& elephant)
-	{
-
-	};
-
-public:
-	GPS * getGps_() const 
-	{
-
-	}
-
-private: // Available methods.
-	Direction getHeading(const Turn turn) const 
-	{
-		//return heading_;
-	}
-
-	//COULD USE EITHER LOOK, WHATEEVER I WANT TO
-	Preserve::Feature look() const
-	{
-
-	}
-	Preserve::Feature look(const Turn turn) const 
-	{
-
-	}
-	int listen() const // Elephants can feel vibrations with their feet. 
-	{
-
-	}
-	void sleep() 
-	{
-
-	}
-	void drink() 
-	{
-
-	}
-	void eat()
-	{
-
-	}
-	void turn(const Turn turn)
-	{
-
-	}
-	void move()
-	{
-
-	}
-
-public: // To be implemented methods. 
-	void tag(const GPS& gps)
-	{
-		//WRITE ONE LINE
-	}
-	void findHerd() 
-	{
-
-	}
-};
+	gps_ = &gps;
+}
+void Elephant::findHerd() 
+{
+		
+	//gps_->move(heading_, 1);
+	
+}
