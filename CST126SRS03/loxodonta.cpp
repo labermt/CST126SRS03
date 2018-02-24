bd7e1c189b6d6cb55fca60c9d5cf6daeff5ca2ec
Drilbur-CST126 <jordan.clark@oit.edu>
2018-02-24 15:35:10 -0800

    Look is not as broken, unlike the elephant's spirits

--- CST126SRS03/loxodonta.cpp
+++ CST126SRS03/loxodonta.cpp
@@ -262,10 +262,130 @@ void Elephant::tag(GPS& gps)
 
 void Elephant::findHerd()
 {
-	Preserve::Feature feature;
+	auto feature = look();
+	while (feature != Preserve::Feature::kHerd)
+	{
+		if (feature == Preserve::Feature::kUnknown)
+		{
+			assert(false); // Let Mitch know
+		}
 
-	feature = look(Turn::k0);
-	feature = look(Turn::kForward);
-	feature = look(Turn::kLeft);
-	feature = look(Turn::kRight);
+		const auto focusDirection = listen();
+		/*if (weight_ - minWeight_ < 80)
+		{
+		if (feature == Preserve::Feature::kGrass)
+		{
+		eat();
+		}
+		else
+		{
+		auto possibleDirections = { Turn::kLeft, Turn::kForward, Turn::kRight };
+		for (auto direction : possibleDirections)
+		{
+		if (look(direction) == Preserve::Feature::kBrush)
+		{
+		turn(direction);
+
+		}
+		}
+		}
+		}*/
+
+		auto heading = getHeading(Turn::kForward);
+		auto lookForward = look(Turn::kForward);
+		auto lookRight = look(Turn::kRight);
+		auto lookLeft = look(Turn::kLeft);
+		auto forwardClear = lookForward == Preserve::Feature::kGrass || lookForward == Preserve::Feature::kDirt || lookForward == Preserve::Feature::kWater || lookForward == Preserve::Feature::kHerd;
+		auto rightClear = lookRight == Preserve::Feature::kGrass || lookRight == Preserve::Feature::kDirt || lookRight == Preserve::Feature::kWater || lookRight == Preserve::Feature::kHerd;
+		auto leftClear = lookLeft == Preserve::Feature::kGrass || lookLeft == Preserve::Feature::kDirt || lookLeft == Preserve::Feature::kWater || lookLeft == Preserve::Feature::kHerd;
+
+		const auto directionDifference = getHeading(Turn::kForward) - focusDirection;
+		switch ((directionDifference + 360) % 360)
+		{
+		case 0:
+			if (!forwardClear)
+			{
+				if (rightClear)
+				{
+					turn(Turn::kRight);
+				}
+				else if (leftClear)
+				{
+					turn(Turn::kLeft);
+				}
+				else
+				{
+					turn(Turn::kRight);
+					turn(Turn::kRight);
+				}
+			}
+			break;
+		case 90:
+			if (leftClear)
+			{
+				turn(Turn::kLeft);
+			}
+			else if (!forwardClear)
+			{
+				turn(Turn::kLeft);
+				lookLeft = look(Turn::kLeft);
+				leftClear = lookLeft == Preserve::Feature::kGrass || lookLeft == Preserve::Feature::kDirt || lookLeft == Preserve::Feature::kWater || lookLeft == Preserve::Feature::kHerd;
+				if (leftClear)
+				{
+					turn(Turn::kLeft);
+				}
+				else
+				{
+					turn(Turn::kRight);
+					turn(Turn::kRight);
+				}
+			}
+			break;
+		case 180:
+			turn(Turn::kRight);
+			turn(Turn::kRight);
+			if (!forwardClear)
+			{
+				if (rightClear)
+				{
+					turn(Turn::kRight);
+				}
+				else if (leftClear)
+				{
+					turn(Turn::kLeft);
+				}
+				else
+				{
+					turn(Turn::kRight);
+					turn(Turn::kRight);
+				}
+			}
+			break;
+		case 270:
+			if (rightClear)
+			{
+				turn(Turn::kRight);
+			}
+			else if (!forwardClear)
+			{
+				turn(Turn::kRight);
+				lookRight = look(Turn::kRight);
+				rightClear = rightClear = lookRight == Preserve::Feature::kGrass || lookRight == Preserve::Feature::kDirt || lookRight == Preserve::Feature::kWater || lookRight == Preserve::Feature::kHerd;
+				if (rightClear)
+				{
+					turn(Turn::kRight);
+				}
+				else
+				{
+					turn(Turn::kRight);
+					turn(Turn::kRight);
+				}
+			}
+			break;
+		default:
+			break;
+		}
+		move();
+		feature = look();
+	}
 }
