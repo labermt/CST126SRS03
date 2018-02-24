b05d1964cc5d0efddca709953326716ad4178838
Drilbur-CST126 <jordan.clark@oit.edu>
2018-02-24 08:27:31 -0800

    Basic code that is probably bad

--- CST126SRS03/elephant.cpp
+++ CST126SRS03/elephant.cpp
@@ -1,3 +1,46 @@
 #include "stdafx.h"
+#include "elephant.h"
 
-// Hi
+void Elephant::tag(GPS& gps)
+{
+	gps_ = &gps;
+}
+
+void Elephant::findHerd()
+{
+	auto feature = look();
+	while (feature != Preserve::Feature::kHerd)
+	{
+		const auto focusDirection = listen();
+		if (weight_ - minWeight_ < 80)
+		{
+			if (feature == Preserve::Feature::kGrass)
+			{
+				eat();
+			}
+			else 
+			{
+				auto possibleDirections = { Turn::kLeft, Turn::kForward, Turn::kRight };
+				for (auto direction : possibleDirections)
+				{
+					if (look(direction) == Preserve::Feature::kBrush)
+					{
+						turn(direction);
+
+					}
+				}
+			}
+		}
+
+		const auto directionDifference = getHeading(Turn::kForward) - focusDirection;
+		if (directionDifference % 360 == 90)
+		{
+			turn(Turn::kLeft);
+		}
+		if (directionDifference % 360 == 270)
+		{
+			turn(Turn::kRight);
+		}
+
+	}
+}
