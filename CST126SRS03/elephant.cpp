2ce39af9fa92a1273ba31ca46b1130fdcb347846
jaredmoit <jared.muralt@oit.edu>
2018-02-25 21:34:26 -0800

    Elephant survival basics complete

--- CST126SRS03/elephant.cpp
+++ CST126SRS03/elephant.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 
+#include <iostream>
 #include "direction.h"
 #include "gps.h"
 #include "preserve.h"
@@ -7,27 +8,93 @@
 
 void Elephant::tag(GPS& gps)
 {
-	gps_ = &gps;
+	gps_ = &gps; //THIS DOESN'T WORK NOW WITH MITCH"S NEW INHERITANCE COMMIT??
 }
-void Elephant::findHerd() 
+void Elephant::findHerd()
 {
 	bool foundHerd{ false };
+	int herdDirection;
+	//Direction direction;
 
 	while (!foundHerd)
 	{
 		//search for herd
 
 		Preserve::Feature feature;
-		
-		feature = look(Turn::k0);
-		feature = look(Turn::kForward);
-		feature = look(Turn::kLeft);
-		feature = look(Turn::kRight);
 
-		//test if herd is found
-		if (look() == Preserve::Feature::kHerd) {
-			foundHerd = true;
+		//FIRST, SURVIVE
+		//Look in all directions for herd
+		Turn turns[4] = { Turn::k0, Turn::kForward, Turn::kLeft , Turn::kRight };
+		for (Turn turn : turns) {
+			feature = look(turn);
+
+			if (feature == Preserve::Feature::kHerd)
+			{
+				foundHerd = true;
+				break;
+			}
 		}
+		feature = look();
+
+		//Deal with hunger
+		if (isHungry() && feature == Preserve::Feature::kGrass) eat(); //eat grass
+		if (isHungry() && look(Turn::kForward) == Preserve::Feature::kBrush) eat(); //eat brush
+
+		//Deal with thirst
+		if (isThirsty() && feature == Preserve::Feature::kWater) drink();
+
+		//Sleep if sleepy
+		if (isSleepy()) sleep();
+
+
+		//SECOND, LISTEN FOR HERD
+		herdDirection = listen();
+
+		//detect if obstacles are in front of elephant
+		if (look(Turn::kForward) == Preserve::Feature::kRock ||
+			look(Turn::kForward) == Preserve::Feature::kBrush)
+		{
+			
+			
+			//TODO: FIGURE OUT HOW TO GO AROUND OBSTACLES
+			
+			
+			//move around obstacle
+			if (herdDirection == 0) {
+				turn(Turn::kRight);
+				turn(Turn::kRight);
+			}
+			else if (herdDirection == 90) {
+				turn(Turn::kRight);
+			}
+			else if (herdDirection == 180) {
+				turn(Turn::kRight);
+				turn(Turn::kRight);
+			}
+			else if (herdDirection == 270) {
+				turn(Turn::kLeft);
+			}
+		}
+		else {
+			//normal move
+			if (herdDirection == 0) {
+				turn(Turn::kForward);
+			}
+			else if (herdDirection == 90) {
+				turn(Turn::kRight);
+			}
+			else if (herdDirection == 180) {
+				turn(Turn::kRight);
+				turn(Turn::kRight);
+			}
+			else if (herdDirection == 270) {
+				turn(Turn::kLeft);
+			}
+		}
+
+		//std::cout << direction << std::endl;
+
+		//LASTLY, MOVE TOWARDS HERD
+		move();
 	}
-	//use look() function to determine what is under your feet
 }
