2ba6915000d9301c80d0460ce8895e265b38977e
TheJonBovi <deraitaa@gmail.com>
2018-03-03 11:50:33 -0800

    fixed encoding issue

--- CST126SRS03/main.cpp
+++ CST126SRS03/main.cpp
@@ -1,21 +1,21 @@
 // main.cpp : Defines the entry point for the console application.
 //
 
-+ACM-include +ACI-stdafx.h+ACI-
-+ACM-include +ADw-iostream+AD4-
-+ACM-include +ACI-elephant.h+ACI-
-+ACM-include +ACI-preserve.h+ACI-
+#include "stdafx.h"
+#include <iostream>
+#include "elephant.h"
+#include "preserve.h"
 
 int main()
-+AHs-
-	// static Preserve preserve+ADs- // TODO: Dynamic preserve. 
+{
+	// static Preserve preserve; // TODO: Dynamic preserve. 
 
-	Elephant elephant(3'500, Direction::kNorth)+ADs-
-	GPS gps(8, 8)+ADs-
-	elephant.tag(gps)+ADs-
-	elephant.findHerd()+ADs-
-	const auto time+AHs- elephant.getElapsedTime() +AH0AOw-
-	std::cout +ADwAPA- +ACI-Herd found in +AD0- +ACI- +ADwAPA- time +ADwAPA- std::endl+ADs-
+	Elephant elephant(6'000, Direction::kNorth);
+	GPS gps(8, 8);
+	elephant.tag(gps);
+	elephant.findHerd();
+	const auto time{ elephant.getElapsedTime() };
+	std::cout << "Herd found in: " << time << std::endl;
 
-	return 0+ADs-
-+AH0-
+	return 0;
+}
